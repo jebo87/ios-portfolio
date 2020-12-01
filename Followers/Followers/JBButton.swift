aed83a361a34cee2b7e8aacc526de93671932fae
Jorge Bautista <jorgebautista@gmail.com>
2020-12-01 10:26:27 -0500

    Search ui layout finished

---
+++ Followers/Followers/JBButton.swift
@@ -0,0 +1,36 @@
+//
+//  JBButton.swift
+//  Followers
+//
+//  Created by Jorge Enrique Bautista Ocampo on 2020-11-30.
+//
+
+import UIKit
+
+class JBButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    //This is only used by storyboards.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String){
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure(){
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false //use autolayout
+    }
+    
+}
---
+++ Followers/Followers/JBTextField.swift
@@ -0,0 +1,47 @@
+//
+//  JBTextField.swift
+//  Followers
+//
+//  Created by Jorge Enrique Bautista Ocampo on 2020-11-30.
+//
+
+import UIKit
+
+class JBTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure(){
+        translatesAutoresizingMaskIntoConstraints = false //to use autolayout
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        
+        placeholder = "Enter a username"
+        
+        
+        
+        
+        
+    }
+    
+}
