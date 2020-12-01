0fef3d8c1acb83b10d8113a735f5f313a610a6c5
Jorge Bautista <jorgebautista@gmail.com>
2020-11-30 22:40:41 -0500

    Followers: initial commit

---
+++ Followers/Followers/FavoritesListVC.swift
@@ -0,0 +1,22 @@
+//
+//  FavoritesLIstVC.swift
+//  Followers
+//
+//  Created by Jorge Enrique Bautista Ocampo on 2020-11-30.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+       
+    }
+    
+
+     
+
+}
---
+++ Followers/Followers/SearchVC.swift
@@ -0,0 +1,21 @@
+//
+//  SearchVC.swift
+//  Followers
+//
+//  Created by Jorge Enrique Bautista Ocampo on 2020-11-30.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+        
+    }
+    
+
+    
+
+}
